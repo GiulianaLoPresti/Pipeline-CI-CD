ed112cd1ad886039a9d05e89e523a6fb0c745945
GiulianaLoPresti <148472695+GiulianaLoPresti@users.noreply.github.com>
2026-02-10 21:52:39 +0000

    My first test

---
+++ CI_CD/CI/test_class.py
@@ -0,0 +1,12 @@
+# Scriviamo una classe semplice che mi permetta di effettuare dei test con i CI/CD
+
+class Testclass:
+    def test_one(self):  # definisco un metodo (test esemplificativo)
+        x="this"
+        assert "h" in x
+
+    def test_two(self):
+        x="hello"
+        assert x =="hello"
+
+# Lo scopo è che questo test venga automatizzato si github, quindi committiamo
